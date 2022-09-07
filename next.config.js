2d2bef548255040db4e8629854a15068580d4e49
Ryan Peh <qwertz0808@gmail.com>
2022-09-08 03:05:05 +0800

    Init project

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
 
-module.exports = nextConfig
+module.exports = withPWA({
+  reactStrictMode: true,
+});
