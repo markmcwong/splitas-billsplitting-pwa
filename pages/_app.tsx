9a48e2a645de6c68c1d7b949cf99b0f34a65518c
Ryan Peh <qwertz0808@gmail.com>
2022-09-09 17:11:15 +0800

    Add basic pages and routing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { orange } from "@mui/material/colors";
+
+// TODO: Properly set up themes, e.g. dark/light mode etc
+
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: orange[300],
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
