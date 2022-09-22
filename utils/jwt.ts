9450065961bed51f76f1ca66105b1a80700c317e
Sun Jia Cheng <sunjc826@gmail.com>
2022-09-22 23:59:48 +0800

    Update JWT to use env

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -1,8 +1,13 @@
 import jsonwebtoken from "jsonwebtoken";
 import * as fs from "fs";
 import * as models from "./models";
-const publicKey = fs.readFileSync("keys/public.pem");
-const privateKey = fs.readFileSync("keys/private.pem");
+// const publicKey = fs.readFileSync("keys/public.pem");
+// const privateKey = fs.readFileSync("keys/private.pem");
+const publicKey = process.env.JWT_PUBLIC_KEY ?? "";
+const privateKey = process.env.JWT_PRIVATE_KEY ?? "";
+if (publicKey === "" || privateKey === "") {
+  throw new Error("JWT key pair missing");
+}
 
 export type TokenPayload = {
   userId: number;
@@ -13,7 +18,7 @@ export type TokenPayload = {
 export function getToken(user: models.User) {
   return jsonwebtoken.sign(
     { userId: user.id, name: user.name, email: user.email },
-    privateKey,
+    Buffer.from(privateKey, "utf-8"),
     {
       algorithm: "RS256",
     }
--- utils/urls.ts
+++ utils/urls.ts
@@ -1,4 +1,7 @@
-export const server = "http://localhost:3000";
+export const server =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://2022-a3-2022-a3-group-10.vercel.app/";
 export const api = `${server}/api`;
 export const oauthRedirect = `${server}/auth`;
 export const homepage = `${server}/user`;
